41b538fe4457d786ac1e6d3ef15b5e7b2bf96fa7
Tsuna <112727506+OwenMillis@users.noreply.github.com>
2022-09-23 19:23:39 +0000

    upd

---
+++ prog54b.py
@@ -0,0 +1,10 @@
+Q = float(input("Please enter first number: "))
+W = float(input("Please enter second number: "))
+E = float(input("Please enter third number: "))
+R = float(input("Please enter fourth number: "))
+
+sum = Q + W + E + R
+average = sum / 4
+
+print("\nThe sum is", sum)
+print("\nThe average is", average)
