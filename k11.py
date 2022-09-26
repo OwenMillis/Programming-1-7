b1d60e2d8a85b4e4040f1ab374e00bf12878f850
Tsuna <112727506+OwenMillis@users.noreply.github.com>
2022-09-26 19:03:01 +0000

    GRAHHHHHHHHHHHFHHHHHHHHHHHGHHHHHHHHHHHHHHH

---
+++ k11.py
@@ -0,0 +1,33 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    move()
+    turn_left()
+    move()
+    put_beeper()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    put_beeper()
+    move()
+    turn_left()
+    move()
+    move()
+    move()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
